0e7e8012bbb0a0ce5a8273bd4eaccf1aa4fa3554
BloobyJoobyJoob <bluepilkintonching@gmail.com>
2023-11-15 15:27:41 +1300

    Setup Basic CSS

--- client/components/LoginPage.tsx
+++ client/components/LoginPage.tsx
@@ -1,8 +1,30 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
+import { FirebaseApp, initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+
 function LoginPage() {
-  return <></>
+  let app: FirebaseApp
+
+  useEffect(() => {
+    const config = {
+      apiKey: 'AIzaSyB4enbUPLq5f3CJnoGSiNIoxV-MLmlAuVQ',
+      authDomain: 'chatgpt-website-c81b2.firebaseapp.com',
+      projectId: 'chatgpt-website-c81b2',
+      storageBucket: 'chatgpt-website-c81b2.appspot.com',
+      messagingSenderId: '348741570396',
+      appId: '1:348741570396:web:dfd779c008b66a00e3fb99',
+      measurementId: 'G-9KW9PBHY99',
+    }
+    app = initializeApp(config)
+  }, [])
+
+  return (
+    <>
+      <div className="container"></div>
+    </>
+  )
 }
 
 export default LoginPage
