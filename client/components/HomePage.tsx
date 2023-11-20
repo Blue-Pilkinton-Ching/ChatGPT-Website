74737c4481eab50927fb01558f12b2962f6c1e18
Blue Pilkinton-Ching <bluepilkintonching@gmail.com>
2023-11-20 14:05:48 +1300

    Add ChatArea Component

--- client/components/HomePage.tsx
+++ client/components/HomePage.tsx
@@ -1,8 +1,10 @@
+import ChatArea from './ChatArea'
 import ThreadPanel from './ThreadsPanel'
 
 export default function HomePage() {
   return (
     <>
+      <ChatArea />
       <ThreadPanel />
     </>
   )
