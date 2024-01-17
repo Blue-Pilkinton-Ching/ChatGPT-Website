a0f799bc0b240ceca0dc9a04e1229c3acd30628f
BloobyJoobyJoob <bluepilkintonching@gmail.com>
2024-01-17 19:29:58 +1300

    readdied for production

--- server/server.ts
+++ server/server.ts
@@ -7,11 +7,13 @@ const httpServer = createServer(app)
 
 app.use(express.json())
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve('dist')))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve('dist', 'index.html'))
-  })
-}
+console.log(process.env.NODE_ENV)
+
+// Uncomment if production
+
+app.use(express.static(path.resolve('dist')))
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('dist', 'index.html'))
+})
 
 export { httpServer }
