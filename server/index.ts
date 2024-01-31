2ede9dd57636de9c733084246ce14eef28d95d93
BloobyJoobyJoob <bluepilkintonching@gmail.com>
2024-01-31 16:24:15 +1300

    fix

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import { httpServer } from './server' // Adjust the import to a named import
 import 'dotenv/config'
 
-const port = process.env.CONNECT_PORT
+const port = process.env.CONNECT_PORT || 80
 
 httpServer.listen(port, function () {
   console.log('Listening on port', port) // Removed eslint-disable-line for brevity
